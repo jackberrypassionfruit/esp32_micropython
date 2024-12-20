79cfe6d5c567f8ab142272041c4042f2b4cb52e7
jackberrypassionfruit <pashayanjack@gmail.com>
2024-12-20 11:31:09 -0500

    idk

---
+++ workSpace/conn_for_mip.py
@@ -0,0 +1,20 @@
+import network
+ 
+def connect():
+  ssid = "TP-Link_1936"
+  password =  "97792710"
+ 
+  station = network.WLAN(network.STA_IF)
+ 
+  if station.isconnected() == True:
+      print("Already connected")
+      return
+ 
+  station.active(True)
+  station.connect(ssid, password)
+ 
+  while station.isconnected() == False:
+      pass
+ 
+  print("Connection successful")
+  print(station.ifconfig())
